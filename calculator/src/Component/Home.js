83bd4b7f4105da9e101871b1e75aa43d26c4e4f1
aak-ritie <aakritibhusal234@gmail.com>
2024-03-12 21:49:30 +0545

    initial

---
+++ calculator/src/Component/Home.js
@@ -0,0 +1,11 @@
+//creating a simple calculator using react hooks
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Simple Calculator</h1>
+      <div className=""></div>
+    </div>
+  );
+}
